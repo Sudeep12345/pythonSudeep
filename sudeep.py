1553fc445ca3530f44782028e7f3b654681738ab
Sudeep12345 <sudeepbudhathoki>
2017-03-17 12:34:07 +0545

    sudeep bhanne file baneko

---
+++ sudeep.py
@@ -0,0 +1,9 @@
+an_integer =12
+a_floating_pint =34
+a_boolean =True 
+a_string = "Foo"
+
+print("Integer value=",an_integer)
+print("float value =",a_floating_pint)
+print("Boolean value =",a_boolean)
+print("string value =",a_string)
