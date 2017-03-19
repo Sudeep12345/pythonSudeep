fd54c06c1e6261f978cbeebc62e8a5c39b61c4eb
Sudeep12345 <sudeepbudhathoki>
2017-03-19 12:27:09 +0545

    my pratice

---
+++ Pratice.py
@@ -0,0 +1,13 @@
+names =['John Doe ','Jane Doe','Johnny Truk']
+
+names[0] = 'Foo Bar'
+print('Names now:', names)
+
+names.append('Molly Mormon')
+names.append('Joe Boggs')
+print('Names finally:',names)
+print('Last name in the list: %s' % names[-1])
+
+joined_name = '\n' .join(names)
+print('\nList of names:')
+print(joined_name)
